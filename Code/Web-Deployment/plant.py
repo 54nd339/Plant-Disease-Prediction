d641604a425d2ca06b4ff8c2ce862fb27bd77636
sandeep.swain <sandeep.swain@jungleegames.com>
2023-11-24 21:26:14 +0530

    done

--- Code/Web-Deployment/plant.py
+++ Code/Web-Deployment/plant.py
@@ -3,20 +3,16 @@ from flask import Flask, render_template, request
 import base64
 import os
 import requests
-from gradio_client import Client
 
-client = Client("https://art3mis011-new-plant-disease-detection.hf.space/--replicas/fm89j/")
 app = Flask(__name__)
 
-# render index.html page
 @app.route("/", methods=['GET', 'POST'])
 def home():
-        return render_template('index.html')
+    return render_template('index.html')
 
-# get input image from client then predict class and render respective .html page for solution
 @app.route("/predict", methods = ['GET','POST'])
 def predict():
-     if request.method == 'POST':
+    if request.method == 'POST':
         file = request.files['image'] # fet input
         app.logger.info(file)
         filename = file.filename  
@@ -25,15 +21,17 @@ def predict():
         with open(file_path, 'rb') as f:
             file_data = base64.b64encode(f.read()).decode('utf-8')
         
-        # Send file name to client
+        file_data = "data:image/jpg;base64," + file_data
+        
         try:
-            result = client.predict({"image": file_data}
-                                    , api_name="/predict")
-            print(result)
+            response = requests.post("https://art3mis011-plantdiseasedetection.hf.space/run/predict", json={
+                "data": [ file_data ]
+            }).json()
+            output = response["data"][0]["label"]
         except Exception as e:
             print("Error: ", e)
 
-        pred, output_page = "Not_a_plant", "Not_a_plant.html" 
+        pred, output_page = output, output + '.html'
         return render_template(output_page, pred_output = pred, user_image = file_path)
     
 # For local system & cloud
